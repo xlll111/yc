82e1034750cc13175bcb50cc6ee6187edb55909c
xl <3010147821@qq.com>
2026-09-04 22:17:08 +0800

    8.2

--- src/views/User.vue
+++ src/views/User.vue
@@ -325,6 +325,9 @@
             <p class="modal-hint small">邮箱：{{ userInfo.email }}</p>
           </div>
           <div v-else-if="modalType === 'shxzhy'">
+            <p class="modal-hint">
+              由于特殊原因，行之慧云暂时无法被访问，恢复时间未知（2026年8月30日）
+            </p>
             <p class="modal-hint">我们将调用行知·慧云的登录接口验证您的身份</p>
             <p class="modal-hint">您的数据只将用于验证身份，您无法使用行知·慧云账号登录YCService</p>
             <label class="modal-label">慧云账号</label>
